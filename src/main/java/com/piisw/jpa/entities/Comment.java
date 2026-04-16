e9664b6e518498150585bf8c6bfa73afed4e00c3
Michał Kaszowski <michal.kaszowski03@gmail.com>
2026-04-16 15:48:49 +0200

    Testy 1-5 działają

--- src/main/java/com/piisw/jpa/entities/Comment.java
+++ src/main/java/com/piisw/jpa/entities/Comment.java
@@ -1,22 +1,22 @@
-package com.piisw.jpa.entities;
-
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@Entity
-public abstract class Comment {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Long id;
-
-    @Column(nullable = false)
-    private String content;
-
-    @ManyToOne
-    @JoinColumn(name = "event_id")
-    private Event event;
-}
+//package com.piisw.jpa.entities;
+//
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@Entity
+//public abstract class Comment {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+//    private Long id;
+//
+//    @Column(nullable = false)
+//    private String content;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "event_id")
+//    private Event event;
+//}
